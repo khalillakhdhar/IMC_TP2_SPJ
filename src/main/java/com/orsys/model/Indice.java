9df7e115d084e3ad8bbb7a110536be234d2c8c95
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-01 11:35:06 +0100

    calcule

--- src/main/java/com/orsys/model/Indice.java
+++ src/main/java/com/orsys/model/Indice.java
@@ -1,5 +1,41 @@
 package com.orsys.model;
 
 public class Indice {
+private double poid,taille;
 
+public double getPoid() {
+	return poid;
+}
+
+public void setPoid(double poid) {
+	this.poid = poid;
+}
+
+public double getTaille() {
+	return taille;
+}
+
+public void setTaille(double taille) {
+	this.taille = taille;
+}
+
+public Indice(double poid, double taille) {
+	super();
+	this.poid = poid;
+	this.taille = taille;
+}
+
+
+public int calculer()
+{
+double a=this.poid/Math.pow(this.taille,2);
+	return (int) Math.round(a);
+
+
+}
+public String result()
+{
+	
+
+}
 }
